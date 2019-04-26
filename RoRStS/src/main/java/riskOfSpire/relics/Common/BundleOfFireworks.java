c0d2dbda32aa25c1d175f6b2380880e9838a571d
Alchyr <IsithAlchyr@gmail.com>
2019-04-26 05:56:55 -0700

    Fireworks and description updating.

---
+++ RoRStS/src/main/java/riskOfSpire/relics/Common/BundleOfFireworks.java
@@ -0,0 +1,54 @@
+package riskOfSpire.relics.Common;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import riskOfSpire.RiskOfSpire;
+import riskOfSpire.actions.general.GuaranteedDamageRandomEnemyAction;
+import riskOfSpire.relics.Abstracts.StackableRelic;
+
+public class BundleOfFireworks extends StackableRelic {
+    public static final String ID = RiskOfSpire.makeID("BundleOfFireworks");
+    private static final int DAMAGE_PER = 3;
+    private static final int DAMAGE_COUNT = 4;
+
+    public BundleOfFireworks() {
+        super(ID, "BundleOfFireworks.png", RelicTier.COMMON, LandingSound.SOLID);
+        this.counter = 0;
+    }
+
+    @Override
+    public void onChestOpen(boolean bossChest) {
+        this.flash();
+        this.counter += DAMAGE_COUNT * relicStack;
+    }
+
+    @Override
+    public void atBattleStart() {
+        this.flash();
+
+        DamageInfo info = new DamageInfo(AbstractDungeon.player, DAMAGE_PER, DamageInfo.DamageType.THORNS);
+
+        for (int i = 0; i < this.counter; ++i) //Hooray for not having to do recursive action.
+        {
+            //Maybe get really dumb and add an actual firework vfx for each firework, so it can look as dumb as it does in ror?
+            AbstractDungeon.actionManager.addToBottom(new GuaranteedDamageRandomEnemyAction(info, AbstractGameAction.AttackEffect.FIRE));
+        }
+        AbstractDungeon.actionManager.addToBottom(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+
+        this.counter = 0;
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        //Whenever you open a chest, deal 3 damage to a random enemy x times at the start of the next combat.
+        //I think displaying final amount is good for this one.
+        return DESCRIPTIONS[0] + DAMAGE_PER + DESCRIPTIONS[1] + DAMAGE_COUNT * relicStack + DESCRIPTIONS[1];
+    }
+
+    public AbstractRelic makeCopy() {
+        return new BundleOfFireworks();
+    }
+}
