7813c3efb36e6f4f82886319e14e87e5205b98b2
erasels <emanuelbedburdick@yahoo.de>
2019-05-03 10:30:39 +0200

    Added Paul's Hoof and art

---
+++ RoRStS/src/main/java/riskOfSpire/relics/Common/PaulsHoof.java
@@ -0,0 +1,41 @@
+package riskOfSpire.relics.Common;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.green.DodgeAndRoll;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.NextTurnBlockPower;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import riskOfSpire.RiskOfSpire;
+import riskOfSpire.relics.Abstracts.StackableRelic;
+
+public class PaulsHoof extends StackableRelic {
+    public static final String ID = RiskOfSpire.makeID("PaulsHoof");
+    private static final int BLOCK_GAIN = 1;
+
+    public PaulsHoof() {
+        super(ID, "PaulsHoof.png", RelicTier.COMMON, LandingSound.SOLID);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0] + getVal() + DESCRIPTIONS[1];
+    }
+
+    public int onPlayerGainedBlock(float blockAmount) {
+        AbstractPlayer p = AbstractDungeon.player;
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new NextTurnBlockPower(p, getVal()), getVal()));
+
+        return MathUtils.floor(blockAmount);
+    }
+
+    private int getVal() {
+        return BLOCK_GAIN*relicStack;
+    }
+
+    public AbstractRelic makeCopy() {
+        return new PaulsHoof();
+    }
+}
