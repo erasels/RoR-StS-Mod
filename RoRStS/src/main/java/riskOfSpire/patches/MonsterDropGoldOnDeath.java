59edc5deb5257b7e27d1fc74c2dc93857b810fbc
lobbienjonsji <robin.sigmund@gmail.com>
2019-05-15 18:50:11 +0200

    Monsters now drop 10% of their Health as gold

---
+++ RoRStS/src/main/java/riskOfSpire/patches/MonsterDropGoldOnDeath.java
@@ -0,0 +1,30 @@
+package riskOfSpire.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireField;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.GainPennyEffect;
+
+public class MonsterDropGoldOnDeath {
+    @SpirePatch(clz = AbstractMonster.class, method = SpirePatch.CLASS)
+    public static class GoldField {
+        public static SpireField<Boolean> hasDroppedGold = new SpireField<>(() -> false);
+    }
+
+    @SpirePatch(clz = AbstractMonster.class, method = "update")
+    public static class MonsterDropGoldOnDeathPatch {
+        @SpirePrefixPatch
+        public static void patch(AbstractMonster __instance) {
+            if (!GoldField.hasDroppedGold.get(__instance) && __instance.isDying) {
+                GoldField.hasDroppedGold.set(__instance, true);
+                int maxhp = __instance.maxHealth / 10;
+                AbstractDungeon.player.gainGold(maxhp);
+                for (int i = 0; i < maxhp; i++) {
+                    AbstractDungeon.effectList.add(new GainPennyEffect(AbstractDungeon.player, __instance.hb.cX, __instance.hb.cY, AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY, true));
+                }
+            }
+        }
+    }
+}
