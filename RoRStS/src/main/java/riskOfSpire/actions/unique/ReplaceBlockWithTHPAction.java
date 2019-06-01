901129f758b3f06d0393a2bdd0853fb0bc7bde5e
erasels <emanuelbedburdick@yahoo.de>
2019-06-01 19:26:46 +0200

    Added Foreign Fruit relic + art

---
+++ RoRStS/src/main/java/riskOfSpire/actions/unique/ReplaceBlockWithTHPAction.java
@@ -0,0 +1,26 @@
+package riskOfSpire.actions.unique;
+
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class ReplaceBlockWithTHPAction extends AbstractGameAction {
+    private static final float DUR = 0.25f;
+
+    public ReplaceBlockWithTHPAction(AbstractCreature target, AbstractCreature source) {
+        this.setValues(target, source);
+        this.actionType = ActionType.BLOCK;
+        this.duration = DUR;
+    }
+
+    public void update() {
+        if (!target.isDeadOrEscaped() && duration == DUR && target.currentBlock > 0) {
+            int tmp = target.currentBlock;
+            target.loseBlock(tmp, false);
+            AbstractDungeon.actionManager.addToTop(new AddTemporaryHPAction(target, source, tmp));
+        }
+
+        this.tickDuration();
+    }
+}
