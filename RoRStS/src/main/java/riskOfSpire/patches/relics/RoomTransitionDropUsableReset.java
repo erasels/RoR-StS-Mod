6683e3c93aa31e363f598207f7e65b56d733a4d9
erasels <emanuelbedburdick@yahoo.de>
2019-08-20 18:21:41 +0200

    UsableDrop status saving complete

--- RoRStS/src/main/java/riskOfSpire/patches/relics/RoomTransitionDropUsableReset.java
+++ RoRStS/src/main/java/riskOfSpire/patches/relics/RoomTransitionDropUsableReset.java
@@ -1,14 +1,15 @@
 package riskOfSpire.patches.relics;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.saveAndContinue.SaveFile;
 import riskOfSpire.util.helpers.RiskOfRainRelicHelper;
 
-@SpirePatch(clz = AbstractDungeon.class, method = "nextRoomTransition")
+@SpirePatch(clz = AbstractDungeon.class, method = "nextRoomTransition", paramtypez = {SaveFile.class})
 public class RoomTransitionDropUsableReset {
     public static void Postfix(AbstractDungeon __instance, SaveFile sf) {
-        if(RiskOfRainRelicHelper.dropUsable) {
+        if(RiskOfRainRelicHelper.dropUsable && !CardCrawlGame.loadingSave) {
             RiskOfRainRelicHelper.dropUsable = false;
         }
     }
