83ac3867c51b48cc94ae8ef066d54e8f6f4ad5f5
erasels <emanuelbedburdick@yahoo.de>
2019-08-21 11:29:10 +0200

    Made GlacialPower functional

--- RoRStS/src/main/java/riskOfSpire/powers/elites/GlacialPower.java
+++ RoRStS/src/main/java/riskOfSpire/powers/elites/GlacialPower.java
@@ -2,17 +2,25 @@ package riskOfSpire.powers.elites;
 
 import basemod.interfaces.CloneablePowerInterface;
 import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.powers.DrawReductionPower;
 import riskOfSpire.RiskOfSpire;
+import riskOfSpire.powers.GainDexPower;
 
 public class GlacialPower extends AbstractElitePower implements CloneablePowerInterface {
     public static final String POWER_ID = RiskOfSpire.makeID("Glacial");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public static final int DRAW_DOWN = 2;
 
     public GlacialPower(AbstractCreature owner) {
         this.name = NAME;
@@ -25,8 +33,20 @@ public class GlacialPower extends AbstractElitePower implements CloneablePowerIn
         tC = Color.SKY.cpy();
     }
 
+    @Override
+    public void onInflictDamage(DamageInfo info, int damageAmount, AbstractCreature target) {
+        if (info.type == DamageInfo.DamageType.NORMAL) {
+            if(damageAmount > 0) {
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, owner, new DrawReductionPower(AbstractDungeon.player, DRAW_DOWN), DRAW_DOWN));
+            } else {
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, owner, new DexterityPower(AbstractDungeon.player, -1), -1));
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, owner, new GainDexPower(AbstractDungeon.player, 1), 1));
+            }
+        }
+    }
+
     public void updateDescription() {
-        this.description = DESCRIPTIONS[0];
+        this.description = DESCRIPTIONS[0] + DRAW_DOWN + DESCRIPTIONS[1];
     }
 
     @Override
