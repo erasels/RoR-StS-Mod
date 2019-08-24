29c303bdfc25d2ede4118dc7d0a923fcb5d993ff
erasels <emanuelbedburdick@yahoo.de>
2019-08-24 21:30:16 +0200

    Made Happiest Mask phantoms not overlap

--- RoRStS/src/main/java/riskOfSpire/patches/relics/HappiestMaskPatches.java
+++ RoRStS/src/main/java/riskOfSpire/patches/relics/HappiestMaskPatches.java
@@ -15,16 +15,19 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.exordium.AcidSlime_L;
 import com.megacrit.cardcrawl.monsters.exordium.SlimeBoss;
 import com.megacrit.cardcrawl.monsters.exordium.SpikeSlime_L;
+import com.megacrit.cardcrawl.powers.MinionPower;
 import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 
 public class HappiestMaskPatches {
     public static boolean hasHM;
     public static boolean victoryMet;
+    private static ArrayList<AbstractMonster> nonPhantomList = new ArrayList<>();
     private static Field skelField;
 
     @SpirePatch(
@@ -34,7 +37,7 @@ public class HappiestMaskPatches {
     public static class RenderNormallyPls {
         @SpirePostfixPatch
         public static void Postfix(AbstractMonster __instance, SpriteBatch sb) throws NoSuchFieldException, IllegalAccessException {
-            if (!victoryMet && (__instance.isDead || __instance.isDying) && !__instance.id.equals(SlimeBoss.ID) && !__instance.id.equals(AcidSlime_L.ID) && !__instance.id.equals(SpikeSlime_L.ID)) {
+            if (!victoryMet && (__instance.isDead || __instance.isDying) && !nonPhantomList.contains(__instance) && !__instance.id.equals(SlimeBoss.ID) && !__instance.id.equals(AcidSlime_L.ID) && !__instance.id.equals(SpikeSlime_L.ID)) {
                 if (hasHM) {
                     if (skelField == null) {
                         skelField = getField(AbstractMonster.class, "skeleton");//AbstractCreature.class.getDeclaredField("skeleton");
@@ -77,6 +80,22 @@ public class HappiestMaskPatches {
             } else {
                 return getField(superClass, fieldName);
             }
+        }
+    }
+
+    @SpirePatch(clz = AbstractMonster.class, method = "die", paramtypez = {boolean.class})
+    public static class NonMinionStacking {
+        public static void Prefix(AbstractMonster __instance, boolean triggerRelics) {
+            if (__instance.hasPower(MinionPower.POWER_ID)) {
+                nonPhantomList.add(__instance);
+            }
+        }
+    }
+
+    @SpirePatch(clz = AbstractMonster.class, method = "useUniversalPreBattleAction")
+    public static class MonsterListClearer {
+        public static void Prefix(AbstractMonster __instance) {
+            nonPhantomList.clear();
         }
     }
 
