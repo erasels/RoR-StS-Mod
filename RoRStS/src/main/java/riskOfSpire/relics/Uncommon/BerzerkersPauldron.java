487ac9a37a5141f9bd0c3f4758f7939c19e2f790
erasels <emanuelbedburdick@yahoo.de>
2019-08-29 17:15:58 +0200

    Added Berzerker's Pauldron + powers and images

---
+++ RoRStS/src/main/java/riskOfSpire/relics/Uncommon/BerzerkersPauldron.java
@@ -0,0 +1,66 @@
+package riskOfSpire.relics.Uncommon;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.MonsterRoom;
+import riskOfSpire.RiskOfSpire;
+import riskOfSpire.powers.relicPowers.BPDAttackPower;
+import riskOfSpire.powers.relicPowers.BPDSkillPower;
+import riskOfSpire.relics.Abstracts.StackableRelic;
+
+public class BerzerkersPauldron extends StackableRelic {
+    public static final String ID = RiskOfSpire.makeID("BerzerkersPauldron");
+    private static final int DOUBLE_PLAY_AMT = 2;
+
+    public BerzerkersPauldron() {
+        super(ID, "BerzerkersPauldron.png", RelicTier.UNCOMMON, LandingSound.SOLID);
+    }
+
+    @Override
+    public void atBattleStart() {
+        if(counter == -2) {
+            stopPulse();
+            flash();
+            counter++;
+            atb(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new BPDAttackPower(AbstractDungeon.player, getVal()), getVal()));
+            atb(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new BPDSkillPower(AbstractDungeon.player, getVal()), getVal()));
+        }
+    }
+
+    @Override
+    public void justEnteredRoom(AbstractRoom r) {
+        if(!(r instanceof MonsterRoom)) {
+            counter = -1;
+            stopPulse();
+        }
+    }
+
+    @Override
+    public void onVictory() {
+        counter = -2;
+        beginLongPulse();
+    }
+
+    @Override
+    public void onLoad(Integer i) {
+        super.onLoad(i);
+        if(counter == -2) {
+            beginLongPulse();
+        }
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0] + getVal()+  DESCRIPTIONS[1];
+    }
+
+    public int getVal() {
+        return DOUBLE_PLAY_AMT + (relicStack-1);
+    }
+
+    public AbstractRelic makeCopy() {
+        return new BerzerkersPauldron();
+    }
+}
